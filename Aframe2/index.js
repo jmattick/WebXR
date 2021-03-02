d84e1b8ed72a524d68ac67ae8311a4e183842f24
Jessica Mattick <jmatty1661@gmail.com>
2021-03-02 00:26:22 -0600

    test

---
+++ Aframe2/index.js
@@ -0,0 +1,12 @@
+$ = (queryString) => document.querySelector(queryString);
+
+const xr = navigator.xr;
+if ("xr" in window.navigator) {
+  /* WebXR can be used! */
+} else {
+  /* WebXR isn't available */
+}
+
+
+
+
