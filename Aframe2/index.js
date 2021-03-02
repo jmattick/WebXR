d08ec9550b181fa7e9cfbf89db07e49082b4214b
Jessica Mattick <jmatty1661@gmail.com>
2021-03-02 14:12:32 -0600

    add moving spheres

--- Aframe2/index.js
+++ Aframe2/index.js
@@ -7,6 +7,37 @@ if ("xr" in window.navigator) {
   /* WebXR isn't available */
 }
 
+const shiftHue = (hue) => (hue + 1) % 360;
+
+let hue = 0;
+
+
+// const animate = () => {
+// 	const spheres = document.querySelectorAll("a-sphere");
+	
+// 	for (i=0; i<spheres.length; i++) {
+// 		let sphere = spheres[i];
+// 		hue = shiftHue(hue);
+// 		const color = `hsl(${hue}, 100%, 50%)`;
+// 		sphere.setAttribute('color', color);
+		
+// 		let pos = sphere.getAttribute('position');
+// 		pos.x += 0.1;
+		
+// 		sphere.setAttribute('position', pos);
+
+		
+// 	}
+// 	requestAnimationFrame(animate());
+	
+
+
+// }
+
+// requestAnimationFrame(animate());
+
+
+
 
 
 
